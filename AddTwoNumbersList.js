628e3080f97aa6a49e8bd00da08d0bea8930947e
shinebayar <shinebayar999@gmail.com>
2022-06-08 13:52:42 +0800

    edited linked list exercise

--- AddTwoNumbersList.js
+++ AddTwoNumbersList.js
@@ -1,43 +1,49 @@
 var addTwoNumbers = function (l1, l2) {
-    let l1_rev = [];
-    for (var i = l1.length - 1; i >= 0; i--) {
-        l1_rev.push(l1[i]);
+
+    let head = new ListNode(0);
+    let node = head;
+    let carry = 0;
+
+    while(l1 || l2){
+        let l1_val = l1 ? l1.val : 0;
+        let l2_val = l2 ? l2.val : 0;
+
+        let sum = l1_val + l2_val + carry;
+        carry = 0;
+        let new_val = sum;
+
+        if(sum>9){
+            new_val = sum % 10;
+            carry = 1;
+        }
+
+        node.next = new ListNode(new_val);
+        node = node.next;
+
+        if(l1){
+            l1 = l1.next;
+        }
+
+        if(l2){
+            l2 = l2.next;
+        }
     }
 
-    let l2_rev = [];
-    for (var i = l2.length - 1; i >= 0; i--) {
-        l2_rev.push(l2[i]);
+    if(carry){
+        node.next = new ListNode(carry);
     }
 
-    let answer = [];
-    let l1_tmp = "";
-    let l2_tmp = "";
-
-    for (var i = 0; i < l1_rev.length; i++) {
-        l1_tmp += l1[i].toString();
-    }
-
-    for (var i = 0; i < l2_rev.length; i++) {
-        l2_tmp += l2[i].toString();
-    }
-
-    var sum = (parseInt(l1_tmp) + parseInt(l2_tmp)).toString();
-
-    for (var i = 0; i < sum.length; i++) {
-        answer.push(sum.charAt(i));
-    }
-
-    let answer1 = [];
-    for( i=answer.length-1; i >=0; i-- ){
-        answer1.push(answer[i]);
-    }
-
-    console.log(answer1);
-
-    return answer1;
+    return head.next;
 };
 
-addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]);
+// class ListNode{
+//     constructor(data){
+//         this.data = data;
+//         this.next = null;
+//     }
+// }
+
+// addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]);
 addTwoNumbers([2, 4, 3], [5, 6, 4]);
 
 // [3,4,2]
