a9471cb8c448d34796628c73012e2d7bc54e0a30
shinebayar <shinebayar999@gmail.com>
2022-06-15 11:22:05 +0800

    Brute force approach - O(n3) - 'longest palindromic string problem'

---
+++ LongestPalindromicString.js
@@ -0,0 +1,54 @@
+// 5. Longest Palindromic Substring
+
+// Share
+// Given a string s, return the longest palindromic substring in s.
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: "bb"
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consist of only digits and English letters.
+
+
+var longestPalindrome = function(s) {
+    const start = Date.now()
+
+    let substrings = []
+
+    for(let i=0; i<s.length; i++){
+        for(let j=i+1; j<s.length+1; j++){
+            substrings.push(s.slice(i,j))
+        }
+    }
+
+    let ans = ''
+    let pre_length = 0
+
+    for(let i=0; i<substrings.length; i++){
+        if(substrings[i] === substrings[i].split('').reverse().join('')){
+            if(substrings[i].length > pre_length){
+                ans = substrings[i]
+                pre_length = substrings[i].length
+            }
+        }
+    }
+
+    console.log(substrings)
+    console.log(ans)
+
+    console.log('Elapsed second: ', Math.floor((Date.now()-start)/1000))
+    return ans
+};
+
+// longestPalindrome('cbbd')
+longestPalindrome("zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir")
