7aedb5159e909ccbb23bcdb6e3fa3b4d3220c4fd
shinebayar <shinebayar999@gmail.com>
2022-06-18 13:31:14 +0800

    tried 'min max game' problem

--- LetterCombinationOfPhoneNumber.js
+++ LetterCombinationOfPhoneNumber.js
@@ -1,29 +1,52 @@
-var letterCombinations = function(digits) {
+var letterCombinations = function (digits) {
   const nums = {
-    2: 'abc',
-    3: 'def',
-    4: 'ghi',
-    5: 'jki',
-    6: 'mno',
-    7: 'pqrs',
-    8: 'tuv',
-    9: 'wxyz',
-  }  
-  let fullString = ''
-  let ans = []
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jki",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+  let ans = [];
+  let all = "";
+  console.log(nums[digits[0]][0])
+  for (let i = 0; i < digits.length; i++) {
+    all += nums[digits[i]];
+  }
 
-  for(let i=0; i<digits.length; i++){
-    for(let j=0; j<nums[digits[i]].length; j++){
-        for(let k=0; k<nums[digits[i+1].length; k++]){
-            ans.push()
+  let str = ''
+
+  for (let j = 0; j < digits.length; j++) {
+    for (let i = 0; i < Object.keys(nums[digits[j]]).length; i++) {
+        if(nums[digits[j+1]][j] !== undefined){
+            firstStr = nums[digits[j]][i]
+            secondStr = nums[digits[j+1]][i]
+            str += (firstStr + secondStr)
+            ans.push(str)
+            firstStr, secondStr = ''
         }
     }
   }
 
-  console.log(ans)
+  console.log(all);
 
-  return ans
+  //   for(let i=0; i<digits.length; i++){
+  //     for(let k=0; k<digits.length; k++){
+  //         start = nums[digits[i]][k]
+  //         if(nums[digits[i+1]] !== undefined){
+  //             for(let j=0; j<Object.keys(nums[digits[i+1]]).length; j++){
+  //                 ans.push((start + nums[digits[i+1]][j]))
+  //             }
+  //         }
+  //     }
+  //   }
 
+  console.log(ans);
+
+  return ans;
 };
 
-letterCombinations('23')
+letterCombinations("23");
+// abc
---
+++ minMaxGame.js
@@ -0,0 +1,21 @@
+var minMaxGame = function(nums) {
+    while(nums.length > 1){
+        let isMin = true
+        let newArr = []
+
+        for(let i=0; i<nums.length; i++){
+            if(isMin){
+                newArr.push(Math.min(nums[i], nums[i+1]))
+            }else{
+                newArr.push(Math.max(nums[i], nums[i+1]))
+            }
+            isMin = ! isMin
+        }
+        nums = newArr
+    }
+
+    console.log(nums)
+    return nums
+};
+
+minMaxGame([1,3,5,2,4,8,2,2])
