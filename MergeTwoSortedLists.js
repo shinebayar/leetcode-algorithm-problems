d367af17af7e1da37bb02516c570e74322c5fe2f
shinebayar <shinebayar999@gmail.com>
2022-06-24 18:48:22 +0800

    #linked-list #recursion @easy -- 21.Merge two sorted lists

---
+++ MergeTwoSortedLists.js
@@ -0,0 +1,129 @@
+// 21. Merge Two Sorted Lists
+
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+ 
+
+// Example 1:
+
+
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+// Example 2:
+
+// Input: list1 = [], list2 = []
+// Output: []
+// Example 3:
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+ 
+
+// Constraints:
+
+// The number of nodes in both lists is in the range [0, 50].
+// -100 <= Node.val <= 100
+// Both list1 and list2 are sorted in non-decreasing order.
+
+const mergeTwoLists = (list1, list2) => {
+    // Initialise a new LinkedList with a dummy ListNode
+    let newList = new ListNode(0);
+
+    // Maintain a reference to the head of the new LinkedList
+    let headOfNewList = newList;
+
+    // Whilst both of the passed in lists contain more elements
+    while (l1 != null && l2 != null) {
+        // If l1's value is smaller
+        if (l1.val < l2.val) {
+            // Add l1's value to the new list
+            newList.next = l1;
+
+            // Move l1 to its next element
+            l1 = l1.next;
+        } else {
+            // Add l2's value to the new list
+            newList.next = l2;
+
+            // Move l2 to its next element
+            l2 = l2.next;
+        }
+
+        // Move into the next level of the LinkedList for the next iteration
+        newList = newList.next;
+    }
+
+    // If l1 has run out of elements
+    if (l1 == null) {
+        // Append l2 to the new list
+        newList.next = l2;
+    }
+    // If l2 has run out of elements
+    else {
+        // Append l1 to the new list
+        newList.next = l1;
+    }
+
+    return headOfNewList.next;
+}
+
+mergeTwoLists([1,2,4], [1,3,4])
+
+class LinkedList{
+    constructor(){
+        this.head = this.tail = null
+    }
+
+    // add to end of list / tail
+    append(value){
+        // if list is empty
+        if(!this.tail){
+            this.head = this.tail = new Node(value)
+        }else{
+            let oldTail = this.tail
+            this.tail = new Node(value)
+            oldTail.next = this.tail
+            this.tail.prev = oldTail
+        }
+    }
+
+    // add to begining of list /head
+    prepend(value){
+        // if list is empty
+        if(!this.head){
+            this.head = this.tail = new Node(value)
+        }else{
+            let oldHead = this.head
+            this.head = new Node(value)
+            
+        }
+    }
+
+    deleteHead(){
+
+    }
+
+    deleteTail(){
+
+    }
+
+    search(){
+
+    }
+}
+
+class Node{
+    constructor(value, prev, next){
+        this.value = value
+        this.prev = prev || null
+        this.next = next || null
+
+    }
+}
+
+// #Linked-list #recursion
+// @easy
