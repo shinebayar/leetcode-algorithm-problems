b4d295cdbf3b06b56eb8de69f58ae48daa53f981
shinebayar <shinebayar999@gmail.com>
2022-06-30 12:44:06 +0800

    #array #math @easy -- 66.Plus one

--- test.js
+++ test.js
@@ -1,8 +1,18 @@
-function recursive(x){
-    if(x == 0)
-    return 0
-    recursive( x - 1 )
+function twoSum(arr, target){
+    let ans = []
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] < target){
+            for(let j=1; j<arr.length; j++){
+                if((target - arr[i]) == arr[i+j]){
+                    ans.push(i)
+                    ans.push(j)
+                }
+            }
+        }
+    }
+
+    console.log(ans)
 
 }
 
-recursive(2)
+twoSum([3,2,4], 6)
