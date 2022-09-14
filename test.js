562d9fd96dabed90428fefdd9d6ae60cf46708f7
shinebayar <shinebayar999@gmail.com>
2022-09-14 20:52:11 +0800

    #two-pointers #string #dynamic-programming -- ##easy -- 392.Is subsequence

--- test.js
+++ test.js
@@ -1,13 +1,21 @@
-const majorityElement = function(arr, target){
-    let left = 0
-    let right = arr.length - 1
-    while( left <= right ){
-        let mid = Math.floor((left + right) / 2)
-        if( arr[mid] === target ) return true
-        if( target > arr[mid] ) left = mid + 1
-        else right = mid - 1
+var isSubsequence = function(s, t) {
+    var s_length = s.length;
+    var t_length = t.length;
+    if(s_length > t_length) return false
+    var dp = [...new Array(s_length +1)].map(ele => new Array(t_length + 1).fill(0) );
+    console.log(dp)
+    
+    for(let i=1; i<=s_length; i++){
+        for(let j=1; j<=t_length; j++){
+            if(s[i-1] === t[j-1]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }else{
+                dp[i][j] = Math.max( dp[i-1][j] , dp[i][j-1])
+            }
+        }
     }
-    return false
-}
+    return dp[s_length][t_length] === s_length
 
-console.log(majorityElement([4,5,9], 4))
+};
+
+console.log(isSubsequence("abc", "ahbgdc"))
