8f1049764e5b63d931f78fd1df10939f61e0d09c
shinebayar <shinebayar999@gmail.com>
2022-09-20 12:28:54 +0800

    #math #bit-manipulation -- ##easy -- 405.Convert a number to hexadecimal

--- ConvertNumberToHexadecimal.js
+++ ConvertNumberToHexadecimal.js
@@ -42,6 +42,7 @@
                10:'a', 11: 'b', 12:'c', 13:'d', 14:'e', 15:'f'}
     let allRemainer = []
     let hex = ''
+    
     if(num > 0){
         while(num >= 1){
             remainer = (num / 16) % 1 * 16
@@ -55,15 +56,94 @@
         }
         
         console.log(allRemainer)
-
+        
         return hex
     }else{
+        let binary = ''
+        console.log('num', num)
+        num = Math.abs(num)
         while(num != 0){
+            if(num % 2 === 0) binary = 0 + binary
+            else binary = 1 + binary
             num = Math.floor(num/2)
         }
+
+        binary = 0 + binary
+        console.log('binary', binary)
+        let flippedBinary = ''
+
+        for(let i=0; i<binary.length; i++){
+            if(binary[i] === '1') flippedBinary += '0'
+            else flippedBinary += '1'
+        }
+
+        console.log('flippedBinary', flippedBinary)
+
+        let carry = 0
+        let addedBinary = ''
+        let added = false
+        for(let i=flippedBinary.length-1; i>=0; i--){
+            if(added && carry === 0){
+                addedBinary = flippedBinary[i] + addedBinary
+                continue
+            }else{
+                if(carry === 0){
+                    if(flippedBinary[i] === '1'){
+                        addedBinary = 0 + addedBinary
+                        carry = 1
+                        added = true
+                    }else {
+                        addedBinary = 1 + addedBinary
+                        added = true
+                    }
+                }else{
+                    if(flippedBinary[i] === '1'){
+                        addedBinary = 0 + addedBinary
+                        carry = 1
+                        added = true
+                    }else {
+                        addedBinary = 1 + addedBinary
+                        carry = 0
+                        added = true
+                    }
+                }
+            }
+        }
+
+        console.log('addedBinary', addedBinary)
+
+
     }
+
     
     
 };
 
-console.log(toHex(26))
+// console.log(toHex(-1))
+
+
+
+
+
+var toHex2 = function(num) {
+    if (num == 0) return '0'
+    let hex = '0123456789abcdef'
+    let res = ''
+    let count = 8 // max number is Math.pow(2,32)-1 , That means length is 32
+
+    while (count && num) {
+        let c = hex[num & 15] // map last 4 bits to a hex digit
+        res = c + res
+        num = num >> 4
+        count--
+    }
+
+    return res
+}
+
+console.log(toHex2(26))
+console.log(toHex2(-1))
+
+
+// #math #bit-manipulation
+// ##easy
