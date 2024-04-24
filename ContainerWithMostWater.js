c3ee844f7a4270c7ed00434b3b1e8698b1131e25
Shinebayar <shinebayar999@gmail.com>
2024-04-24 17:37:10 +0800

    updated -- 11. Container with most water

--- ContainerWithMostWater.js
+++ ContainerWithMostWater.js
@@ -78,21 +78,17 @@ var maxArea = function(height) {
 // Time complexity = O(n)
 // Space complexity = O(1)
 var maxArea = function(height) {
-    let res = 0
-    let h, w
-    
-    let i = 0
-    let j = height.length - 1
-    
-    while(i < j){
-        h = Math.min(height[i], height[j])
-        w = j - i
-        res = Math.max(res, h * w)
-        if(height[i] < height[j]) i++
-        else j--
-    }
-    
-    return res
+    let area = 0, i = 0, j = height.length - 1, w, h
+
+   while(i < j){
+    h = Math.min(height[i], height[j])
+    w = j - i
+    area = Math.max(area, h * w)
+    if(height[i] > height[j]) j--
+    else i++
+   }
+
+   return area
 };
 
 
