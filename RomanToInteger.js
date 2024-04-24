5f25e08941735c789ecd6b7b2e8dc8698aecf82c
Shinebayar <shinebayar999@gmail.com>
2024-04-24 20:36:32 +0800

    updated -- 13. Roman to integer

--- RomanToInteger.js
+++ RomanToInteger.js
@@ -54,20 +54,22 @@
 
 // Approach: hash-table DS
 // Time complexity = O(n)
-// Space complexity = O(n)
+// Space complexity = O(1)
 var romanToInt1 = function(s) {
-    let hash = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000, 
-               'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'CM':900}
-    let toInteger = 0
-    
-    for(let i=0; i<s.length; i++){
-        if(hash[s[i] + s[i+1]] !== undefined){
-            toInteger += hash[s[i] + s[i+1]]
+    let map = new Map([['I', 1], ['V', 5], ['X', 10], ['L', 50], ['C', 100], ['D', 500], ['M', 1000], ['IV', 4], ['IX', 9], ['XL', 40], ['XC', 90], ['CD', 400], ['CM', 900]]);
+    let res = 0, i = 0
+
+    while(i < s.length){
+        if(map.has(s[i] + s[i+1])){
+            res += map.get(s[i] + s[i+1])
+            i = i + 2
+        }else{
+            res += map.get(s[i])
             i++
-        }else toInteger += hash[s[i]]
+        } 
     }
-    
-    return toInteger
+
+    return res
 };
 
 // console.log(romanToInt1("MCMXCIV"))
