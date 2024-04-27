ceda7bb115d4bb0f4f317b1cbb8225e7ee201aca
Shinebayar <shinebayar999@gmail.com>
2024-04-27 18:37:58 +0800

    updated -- 15. 3 sum

--- 3Sum.js
+++ 3Sum.js
@@ -89,27 +89,28 @@
 // Time complexity = O(n2)
 // Space complexity = O(n)
 var threeSum = function(nums) {
-    nums.sort((a, b) => a - b)
-    let res = []
-    
+    nums = nums.sort((a, b) => a - b)
+
+    let target, left, right, res = []
+
     for(let i=0; i<nums.length; i++){
-        if(i > 0 && nums[i] === nums[i-1]) continue
-        let target = 0 - nums[i]
-        let left = i + 1
-        let right = nums.length - 1
+        if(i > 0 && nums[i] === nums[i - 1]) continue
+        target = 0 - nums[[i]]
+        left = i + 1
+        right = nums.length - 1
         while(left < right){
-            if(nums[left] + nums[right] > target) right--
-            else if(nums[left] + nums[right] < target) left++
-            else {
+            if(nums[left] + nums[right] === target){
                 res.push([nums[i], nums[left], nums[right]])
                 while(nums[left] === nums[left + 1]) left++
                 while(nums[right] === nums[right - 1]) right--
                 left++
                 right--
-            }
+            } else if(nums[left] + nums[right] > target){
+                right--
+            }else left++
         }
     }
-        
+    
     return res
 };
 
